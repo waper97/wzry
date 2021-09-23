9b1be36741fd27bc10041f12aae0e12bee809231
waper97 <563667426@qq.com>
2021-09-23 22:27:44 +0800

    瞎几把提交的

---
+++ honor_of_king_data_analysis/src/main/java/com/waper/honor_of_king_data_analysis/controller/MicroBlogController.java
@@ -0,0 +1,32 @@
+package com.waper.honor_of_king_data_analysis.controller;
+
+import org.apache.http.client.HttpClient;
+
+/**
+ * @ClassName MicroBlogController
+ * @Description 微博控制器
+ * @Author wangpeng
+ * @Date 2021/8/7 20:22
+ */
+public class MicroBlogController {
+
+
+
+    String commontUrl = "https://www.weibo.com/aj/v6/comment/small?ajwvr=6&act=list&mid=4667646651401867&uid" +
+                        "=3153019022&isMain=true&dissDataFromFeed=%5Bobject%20Object%5D&ouid=7331797688&location=" +
+                        "v6_content_home&group_source=group_all&rid=4_0_8_5046836015921234974_8_1_0&_t=0&__rnd=1628339064141";
+
+    public Object getComment(){
+
+        return null;
+    }
+
+    public static String sendGet(){
+
+        HttpClient httpClient;
+
+        return "";
+    }
+
+
+}
--- honor_of_king_data_analysis/src/main/java/com/waper/honor_of_king_data_analysis/util/StaticUtil.java
+++ honor_of_king_data_analysis/src/main/java/com/waper/honor_of_king_data_analysis/util/StaticUtil.java
@@ -31,4 +31,19 @@ public class StaticUtil {
      *英雄铭文
      */
     public static final String HERO_POSY  = "https://pvp.qq.com/web201605/js/ming.json";
+
+
+
+    private static String ACCEPT = "accept";
+    private static String ACCEPT_LANGUAGE = "accept-language";
+    private static String CACHE_CONTROL = "cache-control";
+    private static String COOKIE = "这块替换成你自己的cookie";
+    private static String SEC_CH_UA = "sec-ch-ua";
+    private static String SEC_CH_UA_MOBILE = "sec-ch-ua-mobile";
+    private static String SEC_FETCH_DEST = "sec-fetch-dest";
+    private static String SEC_FETCH_MODE = "sec-fetch-mode";
+    private static String SEC_FETCH_SITE = "sec-fetch-site";
+    private static String UPGRADE_INSECURE_REQUESTS = "upgrade-insecure-requests";
+    private static String user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36";
+
 }
